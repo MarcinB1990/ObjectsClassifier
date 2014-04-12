21f753f5752d842dd415b6adb5804a2299b185ae
Marcin Borowiecki <qazswedx@gmail.com>
2014-04-12 09:35:49 +0200

    Dodanie TrainingSetEntity

---
+++ ObjectClassifier/WebRole/Models/TrainingSetEntity.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.WindowsAzure.Storage.Table.DataServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebRole.Models
+{
+    //PartitionKey is UserId, RowKey is TrainingSetId
+    public class TrainingSetEntity:TableServiceEntity
+    {
+        string Name { get; set; }
+        DateTime DateOfEntry { get; set; }
+        int NumberOfClasses{get;set;}
+        int NumberOfAttributes{get; set;}
+        string Comment { get; set; }
+        string TrainingSetFileSource { get; set; }
+        int NumberOfUses{get;set;}
+
+        public TrainingSetEntity()
+            : base(string.Empty,Guid.NewGuid().ToString())
+        {
+        }
+
+        public TrainingSetEntity(string userId,string name,int numberOfClasses,int numberOfAttributes,string comment,string trainingSetFileSource)
+            : base(userId,Guid.NewGuid().ToString())
+        {
+            this.Name = name;
+            this.DateOfEntry = DateTime.Now;
+            this.NumberOfClasses = numberOfClasses;
+            this.NumberOfAttributes = numberOfAttributes;
+            this.Comment = comment;
+            this.TrainingSetFileSource = trainingSetFileSource;
+            this.NumberOfUses = 0;
+        }
+    }
+}
