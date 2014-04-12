ee96d09dfcad3ec79839eea0b4322a4784ed5667
Marcin Borowiecki <qazswedx@gmail.com>
2014-04-12 09:29:12 +0200

    Utworzenie ResultSetEntity

---
+++ ObjectClassifier/WebRole/Models/ResultSetEntity.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.WindowsAzure.Storage.Table.DataServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebRole.Models
+{
+    //PartitionKey is UserId, RowKey is ResultSetId
+    public class ResultSetEntity:TableServiceEntity
+    {
+        string Name { get; set; }
+        DateTime DateOfEntry { get; set; }
+        string Comment { get; set; }
+        string TrainingSetFileSource { get; set; }
+        string InputFileSource { get; set; }
+        string ResultSetFileSource { get; set; }
+        string Progress { get; set; }
+
+        public ResultSetEntity()
+            : base(string.Empty,Guid.NewGuid().ToString())
+        {
+        }
+
+        public ResultSetEntity(string userId,string name,string comment,string trainingSetFileSource,string inputFileSource,string resultSetFileSource)
+            : base(userId,Guid.NewGuid().ToString())
+        {
+            this.Name = name;
+            this.DateOfEntry = DateTime.Now;
+            this.Comment = comment;
+            this.TrainingSetFileSource = trainingSetFileSource;
+            this.InputFileSource = inputFileSource;
+            this.ResultSetFileSource = resultSetFileSource;
+            this.Progress = "To do";
+        }
+    }
+}
