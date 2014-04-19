c855d9227877a21ec9ad97b0c2eb3c96f3acbb7c
Marcin Borowiecki <borowiecki.marcin@gmail.com>
2014-04-19 12:04:29 +0200

    poprawiony skrypt od wyboru tylko jednego radio buttona

--- ObjectClassifier/WebRole/Scripts/SingleRadio.js
+++ ObjectClassifier/WebRole/Scripts/SingleRadio.js
@@ -1,7 +1,7 @@
 ﻿function SelectOne(rdo, gridName) {
     all = document.getElementsByTagName("input");
-    for (i = 2; i < all.length; i++) {
-        if (all[i].type == "radio"){
+    for (i = 0; i < all.length; i++) {
+        if (all[i].type == "radio" && all[i].id != 'MainContent_radioNewOrOldTrainingSet_0' && all[i].id != 'MainContent_radioNewOrOldTrainingSet_1') {
                 all[i].checked = false;
         }
     }
